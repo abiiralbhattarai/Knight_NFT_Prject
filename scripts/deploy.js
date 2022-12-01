6c2a2e8f149c506ceea7650f60241c89f004d14a
abiiralbhattarai <bhattarai.abi07@gmail.com>
2022-12-01 09:22:41 +0545

    Deploy contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,26 +1,78 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const baseURI = "https://www.pinata.cloud/";
+  const _royaltyFeesInBips = 1000;
+  const _royaltyReceiver = "0x78C206B6d21a5DAd5585803570D8555f21071C8c";
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  //for Squires Nft
+  const squiresNft = await hre.ethers.getContractFactory("SquiresNft");
+  const squiresNftContract = await squiresNft.deploy(
+    baseURI,
+    _royaltyFeesInBips,
+    _royaltyReceiver
   );
+
+  await squiresNftContract.deployed();
+
+  //for Other Gen Knights
+  const KnightNft = await hre.ethers.getContractFactory("KnightsNft");
+  const KnightNftContract = await KnightNft.deploy(
+    squiresNftContract.address,
+    _royaltyFeesInBips,
+    _royaltyReceiver
+  );
+  await KnightNftContract.deployed();
+
+  //for Timelock Contract
+  const min_Delay = 2;
+  const proposersAddresses = ["0x78C206B6d21a5DAd5585803570D8555f21071C8c"];
+  const ExecutorAddresses = ["0x78C206B6d21a5DAd5585803570D8555f21071C8c"];
+  const NumberOfConfirmation = 1;
+  const KnightExecutor = await hre.ethers.getContractFactory("KnightExecutor");
+  const KnightExecutorContract = await KnightExecutor.deploy(
+    min_Delay,
+    proposersAddresses,
+    ExecutorAddresses,
+    NumberOfConfirmation
+  );
+  await KnightExecutorContract.deployed();
+
+  //for Governance Contract
+  const KnightGovernance = await hre.ethers.getContractFactory(
+    "KnightGovernance"
+  );
+  const KnightGovernanceContract = await KnightGovernance.deploy(
+    KnightNftContract.address,
+    KnightExecutorContract.address
+  );
+  await KnightGovernanceContract.deployed();
+
+  //for Armours Market Token Contract
+  const ArmoursMarket = await hre.ethers.getContractFactory("ArmoursMarket");
+  const ArmoursMarketContract = await ArmoursMarket.deploy(
+    KnightNftContract.address
+  );
+  await ArmoursMarketContract.deployed();
+
+  //for MagicArmours Token Contract
+  const ArmoursToken = await hre.ethers.getContractFactory("ArmourTokens");
+  const ArmoursTokenContract = await ArmoursToken.deploy(
+    ArmoursMarketContract.address
+  );
+  await ArmoursTokenContract.deployed();
+
+  // printing the address
+  console.log(`Squires_NFT Contract: ${squiresNftContract.address}`);
+  console.log(` Knight_NFT Contract: ${KnightNftContract.address}`);
+  console.log(` Knight_Executor Contract: ${KnightExecutorContract.address}`);
+  console.log(
+    ` Knight_Governance Contract: ${KnightGovernanceContract.address}`
+  );
+  console.log(
+    ` ArmoursToken Contract: ${ArmoursTokenContract.address}`
+  );
+  console.log(` ArmoursMarket Contract: ${ArmoursMarketContract.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
